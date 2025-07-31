6d923e0c2b67dbb3e61a5460198079979fa8e5d4
swapna1525 <siramdasunagaswapna@gmail.com>
2025-08-01 00:35:40 +0530

    Add project source files

---
+++ app/src/main/java/com/androidprojects/mywishcraft/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.androidprojects.mywishcraft
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+
+
